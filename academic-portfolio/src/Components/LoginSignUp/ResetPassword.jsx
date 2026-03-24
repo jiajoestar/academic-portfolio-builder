f4eaed29140cace620bdf7894d1164d2a57a5b6e
Jia Li Lin <emilyshaven@outlook.com>
2026-03-24 14:16:11 +0000

    refined 'reset password' page

--- academic-portfolio/src/Components/LoginSignUp/ResetPassword.jsx
+++ academic-portfolio/src/Components/LoginSignUp/ResetPassword.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState } from 'react';
+import './LoginSignUp.css'
 
 const ResetPassword = () => {
   const { token } = useParams();
@@ -18,17 +19,27 @@ const ResetPassword = () => {
   };
 
   return (
-    <div>
-      <h2>Reset Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='password'
-          placeholder="New password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type='submit'>Reset Password</button>
-      </form>
+    <div className='login-page'>
+      <div className='login-container'>
+        <div className='login-box'>
+          <div className='header'>
+            <h2>Reset Password</h2>
+          </div>
+
+          <form className='inputs' onSubmit={handleSubmit}>
+            <div className='input'>
+              <input
+                type='password'
+                placeholder="New password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+    
+            <button className='login-button' type='submit'>Reset Password</button>
+        </form>
+        </div>
+      </div>
     </div>
   );
 }
