4c1c6cb3bd3f378ae14cb8d8a480fe5de88d9508
Jia Li Lin <emilyshaven@outlook.com>
2026-04-26 01:39:20 +0100

    fixed reset pw url

--- academic-portfolio/server/routes/auth.js
+++ academic-portfolio/server/routes/auth.js
@@ -104,7 +104,7 @@ router.post('/forgot-password', async (req, res) => {
     user.resetTokenExpiry = Date.now() + 15 * 60 * 1000
     await user.save()
 
-    const resetLink = `${import.meta.env.VITE_API_URL}/reset-password/${token}`
+    const resetLink = `${process.env.FRONTEND_URL}/reset-password/${token}`
 
     await sendEmail(
       email,
